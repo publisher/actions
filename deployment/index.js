b4034216c3348c4074d99755e38f6937f39bad1c
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-05-02 15:08:43 -0700

    Add npm auth setup

--- deployment/index.js
+++ deployment/index.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const setupAuth = require("@publisher/npm-auth");
 const deploymentHandler = require("@publisher/deployment-handler");
 
-const token = process.env.GITHUB_TOKEN;
+const npmToken = process.env.NPM_AUTH_TOKEN;
+const ghToken = process.env.GITHUB_TOKEN;
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 
-deploymentHandler(token, eventPayload).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+setupAuth(npmToken)
+  .then(() => deploymentHandler(ghToken, eventPayload))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
