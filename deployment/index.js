05b981089c1272d294293b0b8ef4e2bcddcf8edb
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-05-23 13:13:13 -0700

    Fix publish script

--- deployment/index.js
+++ deployment/index.js
@@ -2,10 +2,15 @@
 
 const deploymentHandler = require("@publisher/deployment-handler");
 
+const setupAuth = require("./auth");
+
+const npmToken = process.env.NPM_AUTH_TOKEN;
 const ghToken = process.env.GITHUB_TOKEN;
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 
-deploymentHandler(ghToken, eventPayload).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+setupAuth(npmToken)
+  .then(() => deploymentHandler(ghToken, eventPayload))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
