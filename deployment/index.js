e63b729f3a9652f307747215c8db79b13df6ab43
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-05-23 12:37:48 -0700

    Update publish script

--- deployment/index.js
+++ deployment/index.js
@@ -1,15 +1,11 @@
 "use strict";
 
-const setupAuth = require("@publisher/npm-auth");
 const deploymentHandler = require("@publisher/deployment-handler");
 
-const npmToken = process.env.NPM_AUTH_TOKEN;
 const ghToken = process.env.GITHUB_TOKEN;
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 
-setupAuth(npmToken)
-  .then(() => deploymentHandler(ghToken, eventPayload))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+deploymentHandler(ghToken, eventPayload).catch(err => {
+  console.error(err);
+  process.exit(1);
+});
