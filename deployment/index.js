70d954cd1affa06071871c1835b551344bcf4458
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-08-23 10:16:59 -0700

    test

--- deployment/index.js
+++ deployment/index.js
@@ -8,6 +8,8 @@ const npmToken = process.env.NPM_AUTH_TOKEN;
 const ghToken = process.env.GITHUB_TOKEN;
 const eventPayload = require(process.env.GITHUB_EVENT_PATH);
 
+console.log(eventPayload);
+
 setupAuth(npmToken)
   .then(() => deploymentHandler(ghToken, eventPayload))
   .catch(err => {
